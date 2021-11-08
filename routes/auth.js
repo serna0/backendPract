2352b6c14ce9e59fd79a92c59bf70fba2dccf7de
Jx <franciscojaviersernavaldez123@gmail.com>
2021-11-07 18:18:21 -0700

    Commit message

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 
-const { login } = require('../controllers/auth');
+const { login } = require('../controllers/auth'); 
 
 
 const router = Router();
